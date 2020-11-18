94c430fbb0c5e9b0c8ecf7f67401585fef4d2e1a
clairedeboer <claire.deboer@gmail.com>
2020-11-17 22:57:09 -0500

    commit tree 2a428e744c4e304787c09ba9daa3766d03de9881

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+     self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    string_array = self.split(" ")
+    new_array = string_array.filter do |word|
+      word.exclamation? || word.sentence? || word.question? 
+     end
+    new_array.count
   end
 end
